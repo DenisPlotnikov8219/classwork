ee9bddf9d6d396f5570305f69a6426a2066c2c1d
Взрослая академия <georgygevorkov@gmail.com>
2026-06-08 18:39:01 +0300

    1

---
+++ 02.06.py
@@ -0,0 +1,57 @@
+#
+# Разложениечисла на разряды
+# num1 = int(input("введите число: "))
+#
+#
+# p1 = num1%10
+# p2 = num1//100
+# p3 = num1%100//10
+# print(p1+p2+p3)
+
+
+#num1 = int(input("введите число: "))
+
+
+#p2 = num1//10%10
+#p3 = num1//100%10
+#p4 = num1//1000
+#print((p1*1000+p2*100+p3*10+p4)*2)
+#
+
+#num1 = int(input())
+
+
+#if num1>0:
+#    print(">0")
+#    print("конец условия")
+
+#print("конец программы")
+
+
+#num1 = int(input())
+#num2 = int(input())
+#if num1>num2:
+#    print("num1")
+#else:
+#   print("num2")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 04.06.py
@@ -0,0 +1,42 @@
+
+# num1 = int(input("введите число: "))
+# num2 = int(input("введите число: "))
+# if num1>num2:
+#     print(num1)
+# if num1<num2:
+#     print(num2)
+# else:
+#     print("=")
+#
+# num1 = int(input("введите растояние: "))
+# num2 = int(input("введите время: "))
+# print (f"{num1/num2}")'''
+#
+#
+# num1 = int(input("введите число: "))
+# num2 = int(input("введите число: "))
+# num3 = int(input("введите число: "))
+# if num1>num2:
+#     if num1>num3:
+#         print ("num1")
+#     elif num1<num2:
+#         if num2<num3:
+#             print ("num3")
+# else:
+#     print ("num2")
+
+# num1 = int(input("введите число: "))
+# if num1<=10 and num1>=0:
+#     print ("да")
+# else:
+#     print ("нет")
+#
+# num1 = int(input("введите число: "))
+# if num1>=0 or num1<=10:
+#     print("да")
+# else:
+#     print ("нет")
+print ("да")
+else:
+    print ("нет")
+
