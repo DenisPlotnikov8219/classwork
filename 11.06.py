bd06e7c71822355ed72c6b5d9affc5c786f2bb04
Взрослая академия <georgygevorkov@gmail.com>
2026-06-11 21:02:55 +0300

    1 циклы+1

---
+++ 11.06.py
@@ -0,0 +1,77 @@
+
+# a = int(input())
+# b = int(input())
+# s = a
+# sum = 0
+#
+# while s <= b:
+#     sum += s
+#     s += 1
+#
+# print(f"Сумма чисел от {a} до {b} равна: {sum}")
+
+#
+# a = int(input())
+# b = int(input())
+#
+# if a>b: a,b=b,a
+# while b>=a:
+#         print(b, end="//")
+#         b-=1
+
+# a1 = int(input())
+# a2 = int(input())
+# b1 = int(input())
+# b2 = int(input())
+# if a1>a2: a1,a2=a2,a1
+# if b1>b2: b1,b2=b2,b1
+# if a1>b1:
+#     min=a1
+# else:
+#     min=b1
+# if a2<b2:
+#     max=a2
+# else:
+#     max=b2
+# while min<=max:
+#     print(min)
+#     min+=1
+
+# a = int(input("Введите число: "))
+# print(a%2 and "не чёт" or "чёт")
+
+# a = int(input("Введите число: "))
+# b = int(input("Введите число: "))
+# if a>b: a,b=b,a
+# for i in range (a, b, 1):
+#     print(i, end="//")
+
+# a = int(input("Введите число: ")) #средне арефметическое
+# b = int(input("Введите число: "))
+# print((a+b)/2)
+#
+# a = int(input("Введите число: "))
+# b = int(input("Введите число: "))
+# if a>b: a,b=b,a
+# for i in range (a, b+1, 3):
+#     print(i, end="//")
+
+
+# a = int(input("Введите число: ")) #
+# b = int(input("Введите число: "))
+# mult=1
+# for i in range (a, b+1):
+#     mult*=i
+# print(mult)
+
+
+#
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+#
+# while b!= 0:
+#  o = b
+#  b = a% b
+#  a = o
+
+# print("Наибольший общий делитель*:", a)
